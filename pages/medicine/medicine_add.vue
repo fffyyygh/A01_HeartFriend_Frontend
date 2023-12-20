61642b44a9e6f4322ff3bbf8c4fde3812eb25bda
fanmengyao <fmy7310@outook.com>
2023-12-20 22:02:44 +0800

    用药备注自动填充

--- pages/medicine/medicine_add.vue
+++ pages/medicine/medicine_add.vue
@@ -19,30 +19,29 @@
 						选择提醒时间
 					</picker>
 
-					
-					
-					 <view v-for="(time, index) in selectedTimes" :key="index" class="time">
-					        <text class="medicine_time">{{ time }}</text>
-							<image class="delete_img" @click="deleteTime(index)" src="/static/medicine/叉号.png"></image>
+
+
+					<view v-for="(time, index) in selectedTimes" :key="index" class="time">
+						<text class="medicine_time">{{ time }}</text>
+						<image class="delete_img" @click="deleteTime(index)" src="/static/medicine/叉号.png"></image>
 					</view>
 				</view>
 			</uni-section>
 			<uni-section title="服药周期" type="line" padding>
-				<uni-datetime-picker v-model="range"  type="daterange"
-					></uni-datetime-picker>
+				<uni-datetime-picker v-model="range" type="daterange"></uni-datetime-picker>
 			</uni-section>
-			
+
 			<uni-section title="下次取药时间" type="line" padding>
-				<uni-datetime-picker v-model="nextPickTime"  type="date"
-					></uni-datetime-picker>
+				<uni-datetime-picker v-model="nextPickTime" type="date"></uni-datetime-picker>
 			</uni-section>
-			
+
 			<uni-section title="服药备注" type="line" padding>
 				<uni-easyinput v-model="note" focus placeholder="请输入服药提醒事项"></uni-easyinput>
 			</uni-section>
-			
+
 			<button class="submit_medicine" @click="submitForm">提交</button>
-			
+			<view style="height: 60rpx;"></view>
+
 		</view>
 	</view>
 </template>
@@ -54,16 +53,16 @@
 				medicineName: "", //药名
 
 				quantity: '', // 左侧输入的数字
-				units: ['粒', '瓶', '毫升','毫克'], // 可选择的单位列表
+				units: ['粒', '瓶', '毫升', '毫克'], // 可选择的单位列表
 				selectedUnit: '单位', // 初始显示的单位
-				
+
 				selectedTimes: [],
 
-				range:[],
-				nextPickTime:"",
-				
-				note:"",
-				
+				range: [],
+				nextPickTime: "",
+
+				note: "",
+
 			}
 		},
 		methods: {
@@ -75,46 +74,50 @@
 				const index = event.detail.value;
 				this.selectedUnit = this.units[index];
 			},
-			
+
 			deleteTime(index) {
-			      this.selectedTimes.splice(index, 1);
-			    },
-			submitForm(){
+				this.selectedTimes.splice(index, 1);
+			},
+			submitForm() {
+				if (this.note === "") {
+					this.note = "无";
+				}
+
 				const dataToSend = {
-					name: this.medicineName,  //名称
-					amount: this.quantity,      // 数量
-					unit: this.selectedUnit,  //单位
-					select_time: this.selectedTimes,  //时间         //周期
-					start_date :this.range[0],
-					finish_date :this.range[1],
-					next_pick_date:this.nextPickTime,  // 取药时间
-					note:this.note,   //用药备注					
+					name: this.medicineName, //名称
+					amount: this.quantity, // 数量
+					unit: this.selectedUnit, //单位
+					select_time: this.selectedTimes, //时间         //周期
+					start_date: this.range[0],
+					finish_date: this.range[1],
+					next_pick_date: this.nextPickTime, // 取药时间
+					note: this.note, //用药备注					
 				};
-				
+
 				const requiredFields = ['name', 'amount', 'unit', 'select_time', "start_date"];
 				const text = {
-					name:"药物名称",
+					name: "药物名称",
 					amount: "剂量",
-					unit:"单位",
-					select_time:"用药时间",
-					start_date:"用药周期"
-					
+					unit: "单位",
+					select_time: "用药时间",
+					start_date: "用药周期"
+
 				}
-				 for (const field of requiredFields) {
-				        if (!dataToSend[field]) {
-				            uni.showToast({
-				                title: `请填写${text[field]}`,
-				                icon: 'none',
-				                duration: 2000
-				            });
-				            return; // 停止提交
-				        }
-				    }
-				  
+				for (const field of requiredFields) {
+					if (!dataToSend[field]) {
+						uni.showToast({
+							title: `请填写${text[field]}`,
+							icon: 'none',
+							duration: 2000
+						});
+						return; // 停止提交
+					}
+				}
+
 				console.log(dataToSend);
 				uni.request({
-					url: 'http://82.157.244.44:8000/api/v1/medicine/',  // 后端接口地址
-					method: 'POST', 
+					url: 'http://82.157.244.44:8000/api/v1/medicine/', // 后端接口地址
+					method: 'POST',
 					header: {
 						'Authorization': `Bearer ${uni.getStorageSync('token')}`,
 					},
@@ -124,10 +127,10 @@
 						console.log(dataToSend);
 						//跳转不了 很奇怪  等后端开发好之后试一下;
 						uni.switchTab({
-							url:"/pages/medicine/medicine"
+							url: "/pages/medicine/medicine"
 						})
-						
-						
+
+
 					},
 					fail: (err) => {
 						console.error('数据发送失败:', err);
@@ -184,41 +187,43 @@
 		align-items: center;
 	}
 
-	
-	
-		
-	.submit_medicine{
-		
+
+
+
+	.submit_medicine {
+		color: white;
 		margin-top: 20rpx;
 		margin-bottom: 60rpx;
 		width: 60%;
 		border-radius: 12px;
 		background-color: #008CBA;
-		box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19);
-		
-	}
-	
-	.time {
-	  display: flex;
+		box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 
-	  justify-content: space-between;
-	  align-items: center;
-	  margin-bottom: 10px;
-	  margin-right: 280rpx;
-	  margin-left: 10rpx;	  
-	    border: 2px solid #008CBA; /* 边框样式 */
-	    border-radius: 5px; /* 可以添加圆角 */
 	}
-	
-	.medicine_time{
-	margin-left: 10rpx;
+
+	.time {
+		display: flex;
+
+		justify-content: space-between;
+		align-items: center;
+		margin-bottom: 10px;
+		margin-right: 280rpx;
+		margin-left: 10rpx;
+		border: 2px solid #008CBA;
+		/* 边框样式 */
+		border-radius: 5px;
+		/* 可以添加圆角 */
 	}
-	
-	
-	
-	.delete_img{
+
+	.medicine_time {
+		margin-left: 10rpx;
+	}
+
+
+
+	.delete_img {
 		width: 20px;
-		height:20px;
+		height: 20px;
 		margin-right: 10rpx;
 	}
 </style>
